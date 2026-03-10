1827e0924574aaac64d47c372c8afc56cdfff614
McCauley Armishaw <151598523+marmishaw14@users.noreply.github.com>
2026-03-09 22:17:18 -0500

    feat(index): start up basic Fastify server

---
+++ apps/api/src/index.ts
@@ -0,0 +1,15 @@
+import Fastify from "fastify";
+
+const app = Fastify({ logger: true });
+
+app.get('/ping', async (request, reply) => {
+    return 'pong\n'
+})
+
+app.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+        app.log.error(err)
+        process.exit(1)
+    }
+    app.log.info(`server is running on ${address}`)
+})
