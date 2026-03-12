6cbb788c827bf2d2ba16d0e6231d7dc8dfba6a2d
marmishaw14 <marmishaw@academic.rrc.ca>
2026-03-11 23:58:19 -0500

    feat(qdrant): start implementing Qdrant client

---
+++ apps/api/src/lib/qdrant.ts
@@ -0,0 +1,7 @@
+import { QdrantClient } from "@qdrant/js-client-rest";
+
+export const qdrant = new QdrantClient({
+    url: "http:localhost",
+    port: 6333,
+    apiKey: process.env.QDRANT_API_KEY,
+});
