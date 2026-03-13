c1ae4e6f4938a59166692bf22aa50d6c2189e25d
marmishaw14 <marmishaw@academic.rrc.ca>
2026-03-13 01:40:42 -0500

    refactor(index): update server start and import search routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,20 @@
 import Fastify from "fastify";
+import { registerSearchRoutes } from "./routes/search";
 
 const app = Fastify({ logger: true });
 
-app.get('/ping', async (request, reply) => {
-    return 'pong\n'
+app.get("/ping", async () => {
+    return "pong\n";
 });
 
-app.listen({ port: 8080 }, (err, address) => {
-    if (err) {
-        app.log.error(err)
-        process.exit(1)
-    }
-    app.log.info(`server is running on ${address}`)
+registerSearchRoutes(app);
+
+async function start() {
+    const address = await app.listen({ port: 8080 });
+    app.log.info(`server is running on ${address}`);
+}
+
+start().catch((err) => {
+    app.log.error(err);
+    process.exit(1);
 });
